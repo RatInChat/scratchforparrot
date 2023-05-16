cc9ab91856acbafae0657b0197e08fea0fcb99b1
RatInChat <vishyfishyalt@gmail.com>
2023-05-16 23:11:41 +0000

    hi
    Signed-off-by: RatInChat <vishyfishyalt@gmail.com>

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const toolbox = {
-    "kind": "flyoutToolbox",
-    "contents": [
-      {
-        "kind": "block",
-        "type": "controls_if"
-      },
-      {
-        "kind": "block",
-        "type": "controls_repeat_ext"
-      },
-      {
-        "kind": "block",
-        "type": "logic_compare"
-      },
-      {
-        "kind": "block",
-        "type": "math_number"
-      },
-      {
-        "kind": "block",
-        "type": "math_arithmetic"
-      },
-      {
-        "kind": "block",
-        "type": "text"
-      },
-      {
-        "kind": "block",
-        "type": "text_print"
-      },
-    ]
-  }
-  const workspace = Blockly.inject('parrot-div', {toolbox: toolbox});
+  "kind": "flyoutToolbox",
+  "contents": [
+    {
+      "kind": "block",
+      "type": "controls_if"
+    },
+    {
+      "kind" : "block",
+      "type" : "logic_compare"
+    },
+    {
+      "kind": "block",
+      "type": "controls_repeat_ext"
+    },
+    {
+      "kind": "block",
+      "type": "math_number",
+      "fields": {
+        "NUM": 123
+      }
+    },
+    {
+      "kind": "block",
+      "type": "math_arithmetic"
+    },
+    {
+      "kind": "block",
+      "type": "text"
+    },
+    {
+      "kind" : "block",
+      "type" : "text_print"
+    }
+  ]
+};
+
+const demoWorkspace = Blockly.inject('blocklyDiv',
+    {media: './node_modules/blockly/media/',
+     toolbox: toolbox});
